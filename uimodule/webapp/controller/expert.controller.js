2ee3f12979f0758e57b5cbc8a7e6ad5e2b9572bd
UISebastian <Sebastian-Marcus.Meier@union-investment.de>
2023-07-17 16:40:28 +0200

    expert.controller.js

--- uimodule/webapp/controller/expert.controller.js
+++ uimodule/webapp/controller/expert.controller.js
@@ -10,8 +10,71 @@ sap.ui.define([
         },
 
         onCreateExpert: function () {
-            //Code for creating a new expert
-            MessageToast.show("Create function not implemented");
+            // Step 1: Define JSONModel instance for new expert
+            this.oNewExpertModel = new JSONModel({
+                Mail: "",
+                Mo: false,
+                Tu: false,
+                We: false,
+                Th: false,
+                Fr: false,
+                ValidFrom: "",
+                ValidTo: ""
+            });
+
+            // Step 2: Load the Fragment
+            if (!this.byId("newExpertDialog")) {
+                Fragment.load({
+                    id: this.getView().getId(),
+                    name: "Service_UI5.view.newExpertDialog",
+                    controller: this
+                }).then(function (oDialog) {
+                    // Connect dialog to the root view of this component (models, lifecycle)
+                    this.getView().addDependent(oDialog);
+                    // Step 3: Set the model
+                    oDialog.setModel(this.oNewExpertModel, "expertCreate");
+                    // Step 4: Open the dialog
+                    oDialog.open();
+                }.bind(this));
+            } else {
+                this.byId("newExpertDialog").open();
+            }
+        },
+
+        onSaveExpert: function () {
+            // Step 1: Get the current data from the JSONModel instance
+            var oData = this.oNewExpertModel.getData();
+
+            // Step 2: Convert Checkbox value to 1 if checked and 0 if unchecked
+            oData.Mo = oData.Mo ? "1" : "0";
+            oData.Tu = oData.Tu ? "1" : "0";
+            oData.We = oData.We ? "1" : "0";
+            oData.Th = oData.Th ? "1" : "0";
+            oData.Fr = oData.Fr ? "1" : "0";
+
+            // Step 3: Map JSONModel data to new object with structure of backend entity
+            var oNewExpert = {
+                Mail: oData.Mail,
+                Mo: oData.Mo,
+                Tu: oData.Tu,
+                We: oData.We,
+                Th: oData.Th,
+                Fr: oData.Fr,
+                ValidFrom: oData.ValidFrom,
+                ValidTo: oData.ValidTo
+            };
+
+            // Step 4: Get ODataModel instance and create new entity
+            var oModel = this.getOwnerComponent().getModel();
+            oModel.create("/zcrm_expert_availabilitySet", oNewExpert, {
+                success: function () {
+                    MessageToast.show("Expert created successfully!");
+                    this.byId("newExpertDialog").close();
+                }.bind(this),
+                error: function () {
+                    MessageToast.show("Error while creating the expert.");
+                }
+            });
         },
 
         onUpdateSelectedExpert: function () {
